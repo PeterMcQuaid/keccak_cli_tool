508eab06627508e204fae1befcdeb5791f75c614
Peter McQuaid <petermcquaid@peters-mbp-2.home>
2025-03-06 21:34:36 +0000

    Implemented correct Keccak256 functionality

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-use std::env;
+use std::{env, fs};
+
+pub mod hash;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+    let mut digests = Vec::new();
 
+    for arg in &args[1..] {
+        match fs::read(arg) {
+            Ok(file) => {
+                digests.push(file);
+            }
+            _ => {
+                digests.push(arg.bytes().collect());
+            }
+        }
+    }
+
+    for digest in &mut digests {
+        println!("{:?}", digest);
+        let hash_result = hash::return_hash(digest);
+        println!("{:?}", hash_result);
+    }
 }
